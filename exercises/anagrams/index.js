7094c186749449bc28538518950e295742152745
Easak Hong <easakhong@gmail.com>
2018-03-31 14:37:08 -0700

    Finish section 9

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,49 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+const anagrams = (strA, strB) => {
+  const sortedA = sortArrToStr(cleanStrToArr(strA));
+  const sortedB = sortArrToStr(cleanStrToArr(strB));
+
+  return sortedA === sortedB;
+};
+
+const cleanStrToArr = str => {
+  return str.toLowerCase().replace(/[^\w]/g, '').split('');
+}
+
+const sortArrToStr = arr => {
+  return arr.sort((a, b) => {
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0;
+  })
+  .join('');
+}
+
+// const anagrams = (strA, strB) => {
+//   const objA = createMap(strA);
+//   const objB = createMap(strB);
+//
+//   if (Object.keys(objA).length !== Object.keys(objB).length) return false;
+//
+//   for (let key in objA) {
+//     if (objA[key] !== objB[key]) {
+//       return false;
+//     }
+//   }
+//
+//   return true;
+// };
+//
+// const createMap = str => {
+//   const obj = {};
+//
+//   for (let char of str.toLowerCase().replace(/[^\w]/g, '')) {
+//     obj[char] = obj[char] + 1 || 1;
+//   }
+//
+//   return obj;
+// }
 
 module.exports = anagrams;
--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,13 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+const chunk = (arr, size) => {
+  const result = [];
+  for (let i = 0; i < arr.length; i += size) {
+    const subArr = arr.slice(i, i + size);
+    result.push(subArr);
+  }
+  return result;
+};
 
 module.exports = chunk;
