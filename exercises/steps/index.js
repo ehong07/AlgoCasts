06783fd6663287b6580a3729a0112f0aa7f4ec1e
Easak Hong <easakhong@gmail.com>
2018-04-01 20:08:48 -0700

    Complete section 11 with recursive approach

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,15 +17,34 @@
 //       '### '
 //       '####'
 
-const steps = n => {
-  for (let i = 0; i < n; i++) {
-    let str = '';
-    const hashtag = '#';
-    const space = ' ';
-    str += hashtag.repeat(i + 1);
-    str += space.repeat(n - i - 1);
-    console.log(str);
+// const steps = n => {
+//   for (let i = 0; i < n; i++) {
+//     let str = '';
+//     const hashtag = '#';
+//     const space = ' ';
+//     str += hashtag.repeat(i + 1);
+//     str += space.repeat(n - i - 1);
+//     console.log(str);
+//   }
+// };
+
+const steps = (n, row = 0, str = '') => {
+  if (n === row) {
+    return;
   }
-};
+
+  if (n === str.length) {
+    console.log(str);
+    return steps(n, row + 1);
+  }
+
+  if (str.length <= row) {
+    str += '#';
+  } else {
+    str += ' ';
+  }
+
+  steps(n, row, str);
+}
 
 module.exports = steps;
