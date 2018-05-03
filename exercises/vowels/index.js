e050eb20fb1cb404c3f7adca68f805fc85cf1244
Easak Hong <easakhong@gmail.com>
2018-05-02 21:33:40 -0700

    Count all vowels in string

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,15 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+const vowels = str => {
+  const vowelArr = ['a', 'e', 'i', 'o', 'u'];
+  let vowelCount = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (vowelArr.indexOf(str[i].toLowerCase()) !== -1) {
+      vowelCount++;
+    }
+  }
+  return vowelCount;
+};
 
 module.exports = vowels;
